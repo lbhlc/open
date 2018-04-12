038bdbec046ae09de1b2defd3718d8eb509b7ef7
lbhlc <76681287@qq.com>
2018-04-12 09:23:34 +0800

    huba

---
+++ AndoridOpengl/app/src/main/java/com/example/administrator/andoridopengl/bean/Point.java
@@ -0,0 +1,20 @@
+package com.example.administrator.andoridopengl.bean;
+
+/**
+ * @author libohan
+ *         邮箱:76681287@qq.com
+ *         create on 2018/4/9.
+ */
+
+public class Point {
+    public float x;
+    public float y;
+    public float z;
+
+    public Point(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+}
